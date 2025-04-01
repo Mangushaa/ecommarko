f43f582211db9b1ee448557c0bb099a6445cbf10
Marko_Popyk <marko.popyk@bt.com>
2025-04-01 13:47:48 +0300

    test gh actions

---
+++ catalog-service/src/test/java/org/ecommarko/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,13 @@
+package org.ecommarko.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
